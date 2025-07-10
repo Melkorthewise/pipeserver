c67b5581e6eaf5160b201ffcb17053062cbd838e
Melkorthewise <76819583+Melkorthewise@users.noreply.github.com>
2025-07-10 21:51:31 +0200

    Windows service VB.NET

---
+++ DSservice.vb
@@ -0,0 +1,45 @@
+﻿Imports System.ServiceProcess
+Imports System.Threading
+
+Public Class DSservice
+    Inherits ServiceBase
+
+    Private _cts As CancellationTokenSource
+    Private server As PipeServerHandler
+
+    Public Sub New()
+        Me.ServiceName = "Service"
+    End Sub
+
+    Protected Overrides Sub OnStart(args() As String)
+        _cts = New CancellationTokenSource()
+        server = New PipeServerHandler(AddressOf Log)
+        Task.Run(Sub() server.Run(_cts.Token))
+        Log("Service started, pipe server running.")
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        If _cts IsNot Nothing Then
+            _cts.Cancel()
+            Log("Service stopping.")
+        End If
+    End Sub
+End Class
+
+Public Module Logger
+    Private ReadOnly SourceName As String = "ServiceSource"
+    Private ReadOnly LogName As String = "Application"
+
+    Public Sub Log(message As String, Optional entryType As EventLogEntryType = EventLogEntryType.Information)
+        Try
+            ' Register once (optional: remove this after installer handles it)
+            If Not EventLog.SourceExists(SourceName) Then
+                EventLog.CreateEventSource(SourceName, LogName)
+            End If
+
+            EventLog.WriteEntry(SourceName, message, entryType)
+        Catch ex As Exception
+            ' Fallback (optional): write to a file or ignore
+        End Try
+    End Sub
+End Module
---
+++ PipeServer.vb
@@ -0,0 +1,69 @@
+﻿Imports System.IO.Pipes
+Imports System.Security.AccessControl
+Imports System.Security.Principal
+Imports System.Text
+Imports System.Threading
+Imports System.Threading.Tasks
+
+Public Class PipeServerHandler
+
+    Private ReadOnly PipeName As String = "TestPipe"
+    Private ReadOnly Log As Action(Of String)
+
+    Public Sub New(logAction As Action(Of String))
+        Log = logAction
+    End Sub
+
+    Public Async Sub Run(ct As CancellationToken)
+        While Not ct.IsCancellationRequested
+            Using pipeServer As New NamedPipeServerStream(PipeName, PipeDirection.InOut, 1,
+                                              PipeTransmissionMode.Message,
+                                              PipeOptions.Asynchronous,
+                                              0, 0,
+                                              CreatePipeSecurity())
+                Log("Waiting for client connection...")
+                Try
+                    Await pipeServer.WaitForConnectionAsync(ct)
+                Catch ex As OperationCanceledException
+                    Log("Pipe server canceled.")
+                    Exit While
+                Catch ex As Exception
+                    Log("WaitForConnection error: " & ex.Message)
+                    Continue While
+                End Try
+
+                Log("Client connected.")
+
+                Try
+                    Dim buffer(255) As Byte
+                    Dim bytesRead = Await pipeServer.ReadAsync(buffer, 0, buffer.Length, ct)
+                    If bytesRead > 0 Then
+                        Dim receivedMsg = Encoding.UTF8.GetString(buffer, 0, bytesRead)
+                        Log("Received from client: " & receivedMsg)
+
+                        ' Respond with a hardcoded string
+                        Dim responseMsg = "Hello from service"
+                        Dim responseBytes = Encoding.UTF8.GetBytes(responseMsg)
+                        Await pipeServer.WriteAsync(responseBytes, 0, responseBytes.Length, ct)
+                        Await pipeServer.FlushAsync(ct)
+                        Log("Sent response to client.")
+                    End If
+                Catch ex As Exception
+                    Log("Communication error: " & ex.Message)
+                End Try
+
+                pipeServer.Disconnect()
+            End Using
+        End While
+        Log("Pipe server stopped.")
+    End Sub
+
+    Private Function CreatePipeSecurity() As PipeSecurity
+        Dim pipeSecurity = New PipeSecurity()
+        pipeSecurity.AddAccessRule(New PipeAccessRule(New SecurityIdentifier(WellKnownSidType.WorldSid, Nothing),
+                                                  PipeAccessRights.FullControl,
+                                                  AccessControlType.Allow))
+        Return pipeSecurity
+    End Function
+
+End Class
